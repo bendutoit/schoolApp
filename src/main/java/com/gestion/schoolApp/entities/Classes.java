dbd450a5511089c9318bcd7275e044e78c29fba1
bendutoit <100129426+bendutoit@users.noreply.github.com>
2022-02-23 17:34:59 +0100

    23/02

--- src/main/java/com/gestion/schoolApp/entities/Classes.java
+++ src/main/java/com/gestion/schoolApp/entities/Classes.java
@@ -17,11 +17,12 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 
 import lombok.NoArgsConstructor;
+import lombok.ToString;
 
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
-
+@ToString
 @Entity
 public class Classes {
    @Id
